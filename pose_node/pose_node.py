56ba1338137947b439241d584480ed2ee80ea026
Luca Boccia <46938039+EllBock@users.noreply.github.com>
2020-11-17 16:01:37 +0100

    Update pose_node.py

--- pose_node/pose_node.py
+++ pose_node/pose_node.py
@@ -3,7 +3,6 @@
 import rospy
 from naoqi import ALProxy
 
-
 rospy.init_node('pose_node')
 
 try:
@@ -24,6 +23,19 @@ rospy.on_shutdown(shutdown)
 rospy.loginfo("Pose Node initialized.")
 postureProxy.goToPosture("StandInit", 1.0)
 rospy.loginfo("Stand Pose reached.")
+
+try:
+    motionProxy = ALProxy("ALMotion", rospy.get_param("/tts_server/pip"), 9559)
+    motionProxy.setStiffnesses("Head", 1.0)
+    names = "HeadYaw"
+    angleLists = [30.0*almath.TO_RAD, 0.0]
+    timeLists  = [1.0, 2.0]
+    isAbsolute = True
+    motionProxy.angleInterpolation(names, angleLists, timeLists, isAbsolute)
+    motionProxy.setStiffnesses("Head", 0.0)
+except Exception, e:
+    rospy.logerr("Cannot move head!")
+
 
 rospy.spin()
 
