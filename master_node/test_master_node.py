ba317d71323f68dedb00db4b3a4830ca27d304b2
Luca Boccia <46938039+EllBock@users.noreply.github.com>
2020-11-19 10:41:40 +0100

    Master small refactor

--- master_node/test_master_node.py
+++ master_node/test_master_node.py
@@ -2,7 +2,6 @@
 
 import os
 import rospy
-from threading import Lock
 import ros_numpy
 from sensor_msgs.msg import Image
 from vision_msgs.msg import Detection2D, Detection2DArray, ObjectHypothesisWithPose
@@ -10,62 +9,7 @@ import cv2
 from pepper_object_detection.classmap import category_map as classmap
 from pepper_object_detection.srv import pepper_tts, pepper_object_detection, pepper_mover
 import random
-
-
-def pepper_say(sentence, timeout=None):
-    try:
-        rospy.wait_for_service('pepper_tts', timeout)
-        tts = rospy.ServiceProxy('pepper_tts', pepper_tts)
-        res = tts(sentence)
-        if not res:
-            rospy.logerr('Unable to use TTS Server')
-    except rospy.ServiceException as e:
-        rospy.logerr("TTS Service call failed: %s"%e)
-    except rospy.ROSException as e:
-        rospy.logwarn("TTS Service request timeout.")
-
-
-def detect_objects(imgmsg, timeout=None):
-    try:
-        rospy.wait_for_service('pepper_object_detection', timeout)
-        det = rospy.ServiceProxy('pepper_object_detection', pepper_object_detection)
-        return det(imgmsg)
-    except rospy.ServiceException as e:
-        rospy.logerr("Object Detection call failed: %s"%e)
-    except rospy.ROSException as e:
-        rospy.logwarn("Object Detection Service request timeout.")
-
-
-def move_head(angleLists, timeLists, timeout=None):
-    try:
-        rospy.wait_for_service('pepper_head_mover', timeout)
-        mover = rospy.ServiceProxy('pepper_head_mover', pepper_mover)
-        res = mover(angleLists, timeLists)
-        if not res:
-            rospy.logerr('Unable to complete head movement.')
-    except rospy.ServiceException as e:
-        rospy.logerr("Move Head call failed: %s"%e)
-    except rospy.ROSException as e:
-        rospy.logwarn("Head Mover Service request timeout.")
-
-
-def objects_sentence(objects: dict, separator=", "):
-    if len(objects) == 0:
-        return "nothing"
-
-    sentence = ""
-    keys = list(objects.keys())
-    random.shuffle(keys)
-    for k in keys:
-        n = objects[k]
-        if n > 1:
-            o = k + "s"
-        else:
-            o = k
-        sentence = sentence + f" {n} {o}" + separator
-
-    return sentence[1:-len(separator)] 
-
+from master_utils import pepper_say, objects_sentence, detect_objects, move_head
 
 
 # Init
@@ -82,7 +26,6 @@ quotes = ["winter is coming", "look, a three headed monkey",
           "never gonna give you up, never gonna let you down",
           "the cake is a lie", "hi pepper 1, i'm pepper 2"]
 pepper_say(random.choice(quotes), 100)
-
 
 
 # Image acquisition
